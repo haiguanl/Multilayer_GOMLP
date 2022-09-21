2e9bfe02a57e37f933f0ddd2a21e495c78adb5fe
Haiguang Liao <haiguanl@andrew.cmu.edu>
2022-09-21 15:23:51 -0400

    Analyze correlation btw gomlp-surrogate with regression

--- utils/analyze_surrogate_gomlp_corr.py
+++ utils/analyze_surrogate_gomlp_corr.py
@@ -1,8 +1,11 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd
+import sklearn
 
 from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+from scipy import stats 
 
 surrogate_results_file = "../surrogate/MLP_ite10000.xlsx"
 gomlp_results_file = "../gomlp/results/gomlp_results_50nets.csv"
@@ -26,11 +29,28 @@ print("gomlp_results: ",len(gomlp_results))
 
 
 
-# plot scatter
 # print("R2: ",r2_score(surrogate_results_list,gomlp_results))
-plt.scatter(gomlp_results,surrogate_results_list,color="r",s=10)
-plt.xlabel("GOMLP")
-plt.ylabel("Surrogate: MLP")
-plt.show()
+# r = np.corrcoef(np.array(gomlp_results).reshape(-1,1),np.array(surrogate_results_list).reshape(-1,1))
+# print("R score: ",r[0,1])
+# correlation study
 
+slope, intercept, r, p, std_err = stats.linregress(surrogate_results_list, gomlp_results)
+print("R score: ",r); print("Slope: ",slope); print("Intercept: ",intercept)
+
+# reg = LinearRegression().fit(
+# 	np.array(surrogate_results_list).reshape(-1,1),\
+# 	np.array(gomlp_results).reshape(-1,1))
+# print("Linear regression R^2: ",\
+# 	reg.score(np.array(surrogate_results_list).reshape(-1,1),\
+# 	np.array(gomlp_results).reshape(-1,1)))
+# print("Linear regression coefficient: ", reg.coef_)
+
+# plot scatter
+plt.scatter(surrogate_results_list,gomlp_results,color="r",s=10)
+plt.xlabel("Surrogate: MLP")
+plt.ylabel("GOMLP")
+plt.plot(np.linspace(min(surrogate_results_list),max(surrogate_results_list),1000),
+	slope*np.linspace(min(surrogate_results_list),max(surrogate_results_list),1000)+intercept,label="R score: "+str(r)[:5])
+plt.legend()
+plt.show()
 
